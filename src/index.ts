73015ae52c7fcba2599e07117d571ce5ea529854
Chintan Shah <chintanshah@promactinfo.com>
2026-01-30 12:46:14 +0530

    feat: define package exports and establish core module entry point.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { run } from '@oclif/core'
 export * from './lib/interfaces/SaasProvider.js';
 export * from './lib/auth/auth-manager.js';
 export * from './lib/config/config-manager.js';
+export * from './lib/http/http-client.js';
+export * from './lib/http/rate-limiter.js';
